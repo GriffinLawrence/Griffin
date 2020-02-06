5d6f7ba032657d3dd84705672b8580efc68fca69
Griffin Lawrence <griffinlawrence@ORRHS.ORG>
2020-02-06 13:56:56 -0500

    Initial

---
+++ Lesson1/App.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(windowWidth,windowHeight)
+    background('green')
+    createP('This was created in java script')
+}
+
+function draw() {
+    createP('This was created in java script')
+}
