d8dcd4d649fd5d713b3c1e6afeabec8bea8f2ae8
Griffin Lawrence <griffinlawrence@ORRHS.ORG>
2020-02-10 12:30:25 -0500

    created clicker game and eddited goblin time

---
+++ Clicker game/App.js
@@ -0,0 +1,21 @@
+let heading 
+let bananaImg
+function setup(){
+    createCanvas(windowWidth, windowHeight)
+    createCanvas(windowWidth, windowHeight)
+    button = createButton('Click for game');
+    button.position(width / 2, height / 2)
+    button.mousePressed(greet)
+    greeting = createElement('h2', 'Catch the banana')
+    greeting.position(width / 2, height / 3)
+    textAlign(CENTER)
+    textSize(20)
+    bananaImg = createImg('/banana.png', 'banana image')
+    bananaImg.size(200, 100)
+    heading =  createElement('h2', 'Click The Banana!')
+    heading.position(width / 3, height / 4)
+}
+let button
+let greeting
+function setup() {
+}
--- Lesson1/App.js
+++ Lesson1/App.js
@@ -1,9 +1,23 @@
-function setup() {
-    createCanvas(windowWidth,windowHeight)
-    background('green')
-    createP('This was created in java script')
-}
 
-function draw() {
-    createP('This was created in java script')
+let button
+let greeting
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  button = createButton('Find out');
+  button.position(width / 2, height / 2)
+  button.mousePressed(greet)
+  greeting = createElement('h2', 'Want to know?')
+  greeting.position(width / 2, height / 3)
+  textAlign(CENTER)
+  textSize(20)
+}
+function greet() {
+  greeting.html('They know where you are, read and run!')
+  for (let i = 0; i < 20; i++) {
+    push();
+    fill(random(255), 255, 255);
+    translate(random(width), random(height))
+    text('All the birds died in 1986 due to raegan killing them and replacing them with spies that are now watching us. The birds work for the bourgeoisie.', 0, 0);
+    pop();
+  }
 }
