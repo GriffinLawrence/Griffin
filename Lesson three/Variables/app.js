f751c6adb51753cba0c1ab8b0a7096c86b2314f9
Griffin Lawrence <griffinlawrence@ORRHS.ORG>
2020-02-12 10:10:38 -0500

    created a consol log game set up

---
+++ Lesson three/Variables/app.js
@@ -0,0 +1,23 @@
+const add = 7 + 3;
+const sub = 7 - 3;
+const mult = 7 * 3;
+const div = 7 / 3;
+const paren = (83 + 92 + 86 + 94) / 4;
+
+let points = 1023;
+let lives = 3;
+let totalTime = 45;
+console.log("Points: " + points);
+console.log("Lives: " + lives);
+console.log("Total Time: " + totalTime + " seconds");
+console.log("---------------------------");
+let totalScore = points*lives/totalTime;
+console.log("Your Total Score is: " + totalScore);
+function.setup(){
+
+    let some ,val = random(1, 6)
+    translate(someVal)
+    console.log("you rolled a: " + someVal)
+    console.log("And a: " + someVal)
+    console.log("For a total of: " ,someVal + someVal)
+}
