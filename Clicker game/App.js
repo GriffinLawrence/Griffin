f450f07b0794219b5b50d0457ce175aeb87d3dae
Griffin Lawrence <griffinlawrence@ORRHS.ORG>
2020-02-13 14:45:22 -0500

    bug fixes

--- Clicker game/App.js
+++ Clicker game/App.js
@@ -1,21 +1,25 @@
-let heading 
+
 let bananaImg
 let button
 let greeting
 function setup(){
     createCanvas(windowWidth, windowHeight)
-    button = createButton('Click for game');
-    button.position(width / 2, height / 2)
-    button.mousePressed(greet)
     greeting = createElement('h2', 'Catch the banana')
     greeting.position(width / 2, height / 3)
     textAlign(CENTER)
     textSize(20)
-    bananaImg = createImg('/banana.png', 'banana image')
+    bananaImg = createImg('banana.png', 'banana image')
     bananaImg.size(200, 100)
-    heading =  createElement('h2', 'Click The Banana!')
-    heading.position(width / 3, height / 4)
+    frameRate(1)
+    bananaImg.mousePressed(youWon)
+   
 }
 function draw(){
     bananaImg.position(random(width), random(height))
+
+}
+function youWon(){
+greeting.html('You Won')
+bananaImg.remove()
+
 }
