da50700fa64bd22bd1b41ef381c7065419c801e0
Griffin Lawrence <griffinlawrence@ORRHS.ORG>
2020-02-27 12:16:49 -0500

    string

---
+++ 54 string methods/app.js
@@ -0,0 +1,35 @@
+let s
+let string = 'my name is griffin             '
+
+//Check Length
+console.log(string.length)
+
+//conversion
+let num = 10
+num.toString()
+console.log(typeof num)
+
+let s = string.search('f')
+
+s = string.startsWith('j')
+
+
+s = string.includes('m')
+
+s = string.toUpperCase()
+
+s = string.toLowerCase()
+
+s = string.concat('and I am great!')
+
+
+s = string.slice(3, 8)
+
+s = string.trim()
+
+
+console.log(s)
+
+num = Number(num)
+console.log(typeof num)
+
