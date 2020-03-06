f16b231762dc3cbc1b4f34bcbb4721b59e736c70
Griffin Lawrence <griffinlawrence@ORRHS.ORG>
2020-03-06 13:59:07 -0500

    mad lads

--- mad libs/app.js
+++ mad libs/app.js
@@ -1,6 +1,7 @@
 let outputDivUI
 let inputDivUI
 
+
 function setup() {
   createCanvas(windowWidth, windowHeight)
   background('lightgray')
@@ -10,27 +11,32 @@ function setup() {
   addTopic(
     'Door',
     'creature one creature two creature three creature four the creatures have lined up at your',
-    ' hands up its the cops'
+    ' hands up its the cops',
+    true,false
   )
   addTopic(
     'place',
     'ten men walked into a bar located in ',
     ' five men walked out ',
+    false,true
   )
   addTopic(
     'Bar name',
     'you were the only witness of the scene that happened in',
     ' tell us all you know or youll be locked up in prison ',
+    false,false
   )
   addTopic(
     'Last Name',
     'Listen',
-    ' We know your game and we can grill you all night long'
+    ' We know your game and we can grill you all night long',
+    false,false
   )
   addTopic(
     'Weapon onomatopoeia',
     'Italian voice: well too bad coppers you wont see sunrise',
-    ' Lets get out of here now boss they will be hot on our tale'
+    ' Lets get out of here now boss they will be hot on our tale',
+    false,false
   )
   // Creates next button and styles it
   let outputDisplayBtn = createButton('NEXT')
@@ -52,24 +58,41 @@ function addInputOutputElements() {
   outputDivUI.hide()
 }
 
-function addTopic(topic, intro, detail) {
+function addTopic(topic, intro, detail, uppercase, num) {
   createElement('p', `Name a ${topic.toUpperCase()}`)
     .id('input-label')
     .parent(inputDivUI)
 
-  let inputFieldUI = createInput(' ')
+  const inputFieldUI = createInput(' ')
     .id('input-field')
     .parent(inputDivUI)
 
   inputFieldUI.changed(updateOutputHandler)
 
-  let output = createP('').parent(outputDivUI)
-
   function updateOutputHandler() {
-    output.html(intro + inputFieldUI.value() + detail)
+    const output = createP('').parent(outputDivUI)
+    // ADD CODE: convert the user input to lowercase by default
+    let userInput = inputFieldUI.value().toLowerCase()
+
+    // ADD CODE: convert to uppercase
+    if (uppercase === true) {
+      // ADD CODE: reassign userInput to be userInput.toUpperCase()
+      userInput = userInput.toUpperCase()
+    }
+
+    // ADD CODE: CONVERT TO NUM
+    
+
+
+    // add if() statement that checks to see iff num === true.
     if(num === true){
       userInput = Number(userInput)
       userInput = userInput+30
     }
+    
+    // if true, convert userInput to a number and perform some math on it
+
+    // OUTPUT BACK TO USER IN THE OUTPUT FIELD
+    output.html(`${intro} ${userInput} ${detail}`)
   }
 }
